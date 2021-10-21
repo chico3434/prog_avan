c4cda392d96abf111013b09068c9e2f8e31d94cf
chico3434 <chicokarkow@hotmail.com>
2021-10-21 15:50:02 -0300

    Modificações sugeridas pelo PyCharm

--- lista2/ex1.py
+++ lista2/ex1.py
@@ -1,9 +1,8 @@
-
 num_criancas = [2, 0, 2, 3, 5, 3, 4, 0, 2, 7, 2, 1, 3, 1, 1, 4, 3, 0, 0, 1]
 
 import statistics as st
 
-print('Média:',st.mean(num_criancas))
+print('Média:', st.mean(num_criancas))
 print('Mediana:', st.median(num_criancas))
 print('Moda:', st.mode(num_criancas))
 print('Variância:', st.variance(num_criancas))

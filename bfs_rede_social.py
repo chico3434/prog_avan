04899f274cb736610c59ba0fba2da1f96cf77029
chico3434 <chicokarkow@hotmail.com>
2021-10-21 15:47:46 -0300

    Modificações sugeridas pelo PyCharm

--- bfs_rede_social.py
+++ bfs_rede_social.py
@@ -4,6 +4,7 @@ Criado em 05/10/2021 para a aula de Programação Avançada
 @author: Francisco Rubens
 """
 
+
 def bfs(g, rotulos, rotulo_inicial):
     """
     Algorirmo BFS
@@ -15,10 +16,10 @@ def bfs(g, rotulos, rotulo_inicial):
     n = len(g)
     no_ini = rotulos.index(rotulo_inicial)
     visitados = [False] * n
-    q = [] # a queue será uma lista
+    q = []  # a queue será uma lista
     q.append(no_ini)
-    hops = [0] * n # lista de hops (número de arestas da origem ao nó)
-    caminho = [] # resultado retornado pela função
+    hops = [0] * n  # lista de hops (número de arestas da origem ao nó)
+    caminho = []  # resultado retornado pela função
     while (q):
         x = q.pop(0)
         if not visitados[x]:
@@ -32,14 +33,15 @@ def bfs(g, rotulos, rotulo_inicial):
 
     return [[rotulos[i], hops[i]] for i in caminho]
 
+
 rotulos = ['Frank', 'Rakesh', 'Eiben', 'Kamber', 'Han']
 
 grafo = [
-    [0,1,0,1,0],
-    [0,0,0,0,0],
-    [0,1,0,0,0],
-    [0,1,0,0,1],
-    [0,0,0,1,0]
+    [0, 1, 0, 1, 0],
+    [0, 0, 0, 0, 0],
+    [0, 1, 0, 0, 0],
+    [0, 1, 0, 0, 1],
+    [0, 0, 0, 1, 0]
 ]
 
 rotulo_no_inicial = 'Han'
